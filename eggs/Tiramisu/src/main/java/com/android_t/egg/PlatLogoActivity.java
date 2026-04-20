42fc0c8f98d5515dcaa7895377b0e860b3230ea7
kqkqku86-a11y <kqkqku86@gmail.com>
2026-04-20 11:53:17 +0700

    Lfinal?2

--- eggs/Tiramisu/src/main/java/com/android_t/egg/PlatLogoActivity.java
+++ eggs/Tiramisu/src/main/java/com/android_t/egg/PlatLogoActivity.java
@@ -38,7 +38,6 @@ import android.graphics.Rect;
 import android.graphics.drawable.LayerDrawable;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
-import android.os.Environment;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.format.Time;
@@ -90,15 +89,24 @@ protected void onCreate(Bundle savedInstanceState) {
 
     getWindow().setNavigationBarColor(0);
     getWindow().setStatusBarColor(0);
+    getWindow().getDecorView().setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
     final ActionBar ab = getActionBar();
     if (ab != null) ab.hide();
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-    if (!Environment.isExternalStorageManager()) {
-        Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-        intent.setData(Uri.parse("package:" + getPackageName()));
-        startActivity(intent);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    if (checkSelfPermission(Manifest.permission.READ_MEDIA_IMAGES)
+            != PackageManager.PERMISSION_GRANTED) {
+        requestPermissions(
+                new String[]{Manifest.permission.READ_MEDIA_IMAGES},
+                REQ_STORAGE
+        );
         return;
     }
 } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -161,8 +169,8 @@ public void onRequestPermissionsResult(int requestCode, String[] permissions, in
             recreate();
         } else {
             new MaterialAlertDialogBuilder(this)
-                    .setTitle("Android 12-13 Easter Egg Permission")
-                    .setMessage("To access the Easter Egg on Android 12/13, you need to allow storage permission. Don't worry, we don't take your data.")
+                    .setTitle("Easter Egg Permission")
+                    .setMessage("To access the Easter Egg, you need to allow the Photos & Media permission. Don't worry, we don't take your data.")
                     .setCancelable(false)
                     .setPositiveButton("OK", (dialog, which) -> {
                         Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
