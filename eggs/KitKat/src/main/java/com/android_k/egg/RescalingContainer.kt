aa9ab9b4b4d6d8863be8ab9e0a30aaa808eb76a3
kqkqku86-a11y <kqkqku86@gmail.com>
2026-04-22 19:00:56 +0700

    Update RescalingContainer.kt

--- eggs/KitKat/src/main/java/com/android_k/egg/RescalingContainer.kt
+++ eggs/KitKat/src/main/java/com/android_k/egg/RescalingContainer.kt
@@ -1,17 +1,17 @@
-package com.android_k.egg
+// package com.android_k.egg
 
-import android.content.Context
-import android.util.AttributeSet
-import android.view.View
-import android.widget.FrameLayout
+// import android.content.Context
+// import android.util.AttributeSet
+// import android.view.View
+// import android.widget.FrameLayout
 
-class RescalingContainer @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null
-) : FrameLayout(context, attrs) {
+// class RescalingContainer @JvmOverloads constructor(
+//    context: Context,
+//    attrs: AttributeSet? = null
+// ) : FrameLayout(context, attrs) {
 
-    fun setView(v: View) {
-        removeAllViews()
-        addView(v)
-    }
-}
+//    fun setView(v: View) {
+//        removeAllViews()
+//        addView(v)
+//    }
+// }
