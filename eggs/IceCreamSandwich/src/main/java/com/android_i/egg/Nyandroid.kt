e77d6b6a9bf32046e7d815eecb3e64353082d4dc
kqkqku86-a11y <kqkqku86@gmail.com>
2026-04-23 12:01:53 +0700

    Add files via upload

--- eggs/IceCreamSandwich/src/main/java/com/android_i/egg/Nyandroid.kt
+++ eggs/IceCreamSandwich/src/main/java/com/android_i/egg/Nyandroid.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2011 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android_i.egg
 
 import android.animation.TimeAnimator
@@ -31,61 +15,31 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.FrameLayout
 import android.widget.ImageView
+import kotlin.math.max
 import kotlin.random.Random
 
 class Nyandroid : Activity() {
 
-    companion object {
-        const val DEBUG = false
-    }
-
-    class Board(context: Context, as: AttributeSet?) : FrameLayout(context, as) {
+    class Board(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
         companion object {
             const val FIXED_STARS = true
             const val NUM_CATS = 20
-
-            val sRNG = Random.Default
-
-            fun lerp(a: Float, b: Float, f: Float): Float {
-                return (b - a) * f + a
-            }
-
-            fun randfrange(a: Float, b: Float): Float {
-                return lerp(a, b, sRNG.nextFloat())
-            }
-
-            fun randsign(): Int {
-                return if (sRNG.nextBoolean()) 1 else -1
-            }
-
-            fun <E> pick(array: Array<E>): E? {
-                if (array.isEmpty()) return null
-                return array[sRNG.nextInt(array.size)]
-            }
+            val rng = Random
         }
 
-        inner class FlyingCat(context: Context, as: AttributeSet?) : ImageView(context, as) {
+        private fun lerp(a: Float, b: Float, f: Float) = (b - a) * f + a
+        private fun rand(a: Float, b: Float) = lerp(a, b, rng.nextFloat())
 
-            companion object {
-                const val VMAX = 1000.0f
-                const val VMIN = 100.0f
-            }
+        inner class FlyingCat(ctx: Context) : ImageView(ctx) {
 
-            var v: Float = 0f
-            var vr: Float = 0f
-            var dist: Float = 0f
-            var z: Float = 0f
+            var v = 0f
+            var dist = 0f
+            var z = 0f
             var component: ComponentName? = null
 
             init {
                 setImageResource(R.drawable.i_nyandroid_anim)
-
-                if (DEBUG) setBackgroundColor(0x80FF0000.toInt())
-            }
-
-            override fun toString(): String {
-                return "<cat (${x}, ${y}) (${width} x ${height})>"
             }
 
             fun reset() {
@@ -93,167 +47,93 @@ class Nyandroid : Activity() {
                 scaleX = scale
                 scaleY = scale
 
-                x = -scale * width + 1
-                y = randfrange(0f, (this@Board.height - scale * height).toFloat())
-                v = lerp(VMIN, VMAX, z)
+                x = -scale * width
+                y = rand(0f, (this@Board.height - height).toFloat())
 
+                v = lerp(100f, 1000f, z)
                 dist = 0f
-
-                Log.d("Nyandroid", "reset cat: $this")
             }
 
             fun update(dt: Float) {
                 dist += v * dt
-                x = x + v * dt
+                x += v * dt
             }
         }
 
-        private var mAnim: TimeAnimator? = null
+        private var anim: TimeAnimator? = null
 
         init {
             setLayerType(View.LAYER_TYPE_HARDWARE, null)
-            systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+            systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             setBackgroundColor(0xFF003366.toInt())
         }
 
         private fun reset() {
-            Log.d("Nyandroid", "board reset")
             removeAllViews()
 
             val wrap = LayoutParams(
-                LayoutParams.WRAP_CONTENT,
-                LayoutParams.WRAP_CONTENT
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
             )
 
             if (FIXED_STARS) {
-                for (i in 0 until 20) {
-                    val fixedStar = ImageView(context)
-                    if (DEBUG) fixedStar.setBackgroundColor(0x8000FF80.toInt())
-                    fixedStar.setImageResource(R.drawable.i_star_anim)
-
-                    addView(fixedStar, wrap)
-
-                    val scale = randfrange(0.1f, 1f)
-                    fixedStar.scaleX = scale
-                    fixedStar.scaleY = scale
-                    fixedStar.x = randfrange(0f, width.toFloat())
-                    fixedStar.y = randfrange(0f, height.toFloat())
-
-                    val anim = fixedStar.drawable as AnimationDrawable
-
-                    for (n in 0 until anim.numberOfFrames) {
-                        val drawable = anim.getFrame(n)
-                        if (drawable is BitmapDrawable) {
-                            drawable.setTargetDensity(480)
-                        }
-                    }
-
-                    postDelayed({
-                        anim.start()
-                    }, randfrange(0f, 1000f).toLong())
+                repeat(20) {
+                    val star = ImageView(context)
+                    star.setImageResource(R.drawable.i_star_anim)
+                    addView(star, wrap)
                 }
             }
 
-            for (i in 0 until NUM_CATS) {
-                val nv = FlyingCat(context, null)
-                addView(nv, wrap)
+            repeat(NUM_CATS) {
+                val cat = FlyingCat(context)
+                addView(cat, wrap)
 
-                nv.z = (i.toFloat() / NUM_CATS)
-                nv.z *= nv.z
+                cat.z = (it.toFloat() / NUM_CATS)
+                cat.z *= cat.z
 
-                nv.reset()
-                nv.x = randfrange(0f, width.toFloat())
-
-                val anim = nv.drawable as AnimationDrawable
-
-                for (n in 0 until anim.numberOfFrames) {
-                    val drawable = anim.getFrame(n)
-                    if (drawable is BitmapDrawable) {
-                        drawable.setTargetDensity(480)
-                    }
-                }
-
-                postDelayed({
-                    anim.start()
-                }, randfrange(0f, 1000f).toLong())
+                cat.reset()
+                cat.x = rand(0f, width.toFloat())
             }
 
-            mAnim?.cancel()
-
-            mAnim = TimeAnimator().apply {
-                setTimeListener { _, totalTime, deltaTime ->
-                    Log.d("Nyandroid", "t=$totalTime")
+            anim?.cancel()
+            anim = TimeAnimator().apply {
+                setTimeListener { _, _, dt ->
+                    val sec = dt / 1000f
 
                     for (i in 0 until childCount) {
                         val v = getChildAt(i)
-                        if (v !is FlyingCat) continue
+                        if (v is FlyingCat) {
+                            v.update(sec)
 
-                        v.update(deltaTime / 1000f)
-
-                        val catWidth = v.width * v.scaleX
-                        val catHeight = v.height * v.scaleY
-
-                        if (v.x + catWidth < -2 ||
-                            v.x > width + 2 ||
-                            v.y + catHeight < -2 ||
-                            v.y > height + 2
-                        ) {
-                            v.reset()
+                            if (v.x > width || v.x < -100) {
+                                v.reset()
+                            }
                         }
                     }
                 }
             }
         }
 
-        private val postStart = Runnable {
-            reset()
-            mAnim?.start()
-        }
-
         override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-            super.onSizeChanged(w, h, oldw, oldh)
-            Log.d("Nyandroid", "resized: ${w}x$h")
-            post(postStart)
+            post {
+                reset()
+                anim?.start()
+            }
         }
 
         override fun onDetachedFromWindow() {
             super.onDetachedFromWindow()
-            removeCallbacks(postStart)
-            mAnim?.cancel()
-        }
-
-        override fun isOpaque(): Boolean {
-            return true
+            anim?.cancel()
         }
     }
 
-    private var mBoard: Board? = null
-
-    override fun onStart() {
-        super.onStart()
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         window.addFlags(
-            WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON or
-                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
         )
-    }
 
-    override fun onResume() {
-        super.onResume()
-
-        mBoard = Board(this, null)
-        setContentView(mBoard)
-
-//        mBoard?.setOnSystemUiVisibilityChangeListener {
-//            if (it and View.SYSTEM_UI_FLAG_HIDE_NAVIGATION == 0) {
-//                finish()
-//            }
-//        }
-    }
-
-    override fun onUserInteraction() {
-        Log.d("Nyandroid", "finishing on user interaction")
-        finish()
+        setContentView(Board(this, null))
     }
 }
--- eggs/IceCreamSandwich/src/main/java/com/android_i/egg/PlatLogoActivity.kt
+++ eggs/IceCreamSandwich/src/main/java/com/android_i/egg/PlatLogoActivity.kt
@@ -1,24 +1,9 @@
-/*
- * Copyright (C) 2010 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android_i.egg
 
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.VectorDrawable
 import android.os.Bundle
@@ -35,7 +20,7 @@ class PlatLogoActivity : Activity() {
 
     private var mToast: Toast? = null
     private lateinit var mContent: ImageView
-    private var mCount: Int = 0
+    private var mCount = 0
 
     private val mHandler = Handler(Looper.getMainLooper())
 
@@ -43,23 +28,26 @@ class PlatLogoActivity : Activity() {
         override fun run() {
             mCount++
 
-            val mZzz = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-            mZzz.vibrate(50L * mCount)
+            val vib = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            vib.vibrate(50L * mCount)
 
             val scale = 1f + 0.25f * mCount * mCount
 
-            val drawable: Drawable? = mContent.drawable
+            val drawable = mContent.drawable
 
             if (drawable is VectorDrawable) {
-                val newWidth = drawable.intrinsicWidth * scale
-                val newHeight = drawable.intrinsicHeight * scale
+                val newW = drawable.intrinsicWidth * scale
+                val newH = drawable.intrinsicHeight * scale
 
-                val left = -((newWidth - drawable.intrinsicWidth) / 2f).toInt()
-                val top = -((newHeight - drawable.intrinsicHeight) / 2f).toInt()
-                val right = (newWidth + left).toInt()
-                val bottom = (newHeight + top).toInt()
+                val left = -((newW - drawable.intrinsicWidth) / 2f).toInt()
+                val top = -((newH - drawable.intrinsicHeight) / 2f).toInt()
 
-                drawable.setBounds(left, top, right, bottom)
+                drawable.setBounds(
+                    left,
+                    top,
+                    (newW + left).toInt(),
+                    (newH + top).toInt()
+                )
             } else {
                 mContent.scaleX = scale
                 mContent.scaleY = scale
@@ -72,14 +60,9 @@ class PlatLogoActivity : Activity() {
                 )
             } else {
                 try {
-                    startActivity(
-                        Intent(this@PlatLogoActivity, Nyandroid::class.java)
-                    )
-                } catch (ex: Exception) {
-                    android.util.Log.e(
-                        "PlatLogoActivity",
-                        "Couldn't find platlogo screensaver."
-                    )
+                    startActivity(Intent(this@PlatLogoActivity, Nyandroid::class.java))
+                } catch (e: Exception) {
+                    android.util.Log.e("PlatLogoActivity", "Couldn't find Nyandroid")
                 }
                 finish()
             }
@@ -99,7 +82,7 @@ class PlatLogoActivity : Activity() {
             setImageResource(R.drawable.i_platlogo)
             scaleType = ImageView.ScaleType.CENTER_INSIDE
 
-            setOnTouchListener { v, event ->
+            setOnTouchListener { _, event ->
                 when (event.action) {
 
                     MotionEvent.ACTION_DOWN -> {
@@ -128,7 +111,6 @@ class PlatLogoActivity : Activity() {
     }
 
     override fun onDestroy() {
-        // fix leak
         mHandler.removeCallbacks(mSuperLongPress)
         super.onDestroy()
     }
